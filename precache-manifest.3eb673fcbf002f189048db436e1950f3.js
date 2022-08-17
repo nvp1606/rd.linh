c7cdf8507967fb18062500907038eb4636133c16
Phuong nv <phuongnv@MacBook-Pro-cua-Phuong.local>
2022-08-17 22:41:10 +0900

    Updates

---
+++ precache-manifest.3eb673fcbf002f189048db436e1950f3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "68d8a9c253608be6a947",
+    "url": "/rd.linh/static/css/main.93f4df5e.chunk.css"
+  },
+  {
+    "revision": "68d8a9c253608be6a947",
+    "url": "/rd.linh/static/js/main.68d8a9c2.chunk.js"
+  },
+  {
+    "revision": "0d630a3e1f21db7817dc",
+    "url": "/rd.linh/static/js/1.0d630a3e.chunk.js"
+  },
+  {
+    "revision": "7d89464741f2193926da",
+    "url": "/rd.linh/static/js/2.7d894647.chunk.js"
+  },
+  {
+    "revision": "c9d4c4867fdb53d98009",
+    "url": "/rd.linh/static/js/runtime~main.c9d4c486.js"
+  },
+  {
+    "revision": "84ec5f078773aa0f11405bc0b4e2b2ad",
+    "url": "/rd.linh/index.html"
+  }
+];
